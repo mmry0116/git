1f154e263b529ea4e7bd834c7a69b88417c34738
mmry <473747139@qq.com>
2023-09-02 11:03:51 +0800

    seven commit4

--- src/main/java/com/mmry/GitTest.java
+++ src/main/java/com/mmry/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
         System.out.println("test");
         System.out.println("hot-fix");
         System.out.println("test2");
-        System.out.println("fix2");
-        System.out.println("test3");
+        System.out.println(" seven commit4");
+
     }
 }
