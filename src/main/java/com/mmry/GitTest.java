2d607f40ab43b1a840e4455b4b00699cd3bda75c
mmry <473747139@qq.com>
2023-09-02 10:42:39 +0800

    second commit

---
+++ src/main/java/com/mmry/GitTest.java
@@ -0,0 +1,9 @@
+package com.mmry;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git init");
+        System.out.println("git add");
+        System.out.println("git commit");
+    }
+}
