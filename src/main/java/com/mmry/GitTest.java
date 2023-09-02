00114c797b28d43ed95a29556223cb03e8f7dd2d
mmry <473747139@qq.com>
2023-09-02 11:58:13 +0800

    night commit4

--- src/main/java/com/mmry/GitTest.java
+++ src/main/java/com/mmry/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("test2");
         System.out.println(" seven commit4");
+        System.out.println("1243333");
 
     }
 }
