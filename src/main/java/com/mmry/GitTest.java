14e70b384e195f96e66b57f0a653468d5120cc77
mmry <473747139@qq.com>
2023-09-02 10:51:50 +0800

    hotfix2

--- src/main/java/com/mmry/GitTest.java
+++ src/main/java/com/mmry/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("git commit");
         System.out.println("test");
         System.out.println("hot-fix");
+        System.out.println("test");
+        System.out.println("fix2");
     }
 }
